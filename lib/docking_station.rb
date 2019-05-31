98d63919dfbd4cadbddf0c334f5876c9d9a2b384
Student <student@makerss-mbp-6.home>
2019-05-31 21:01:32 +0100

    Capacity Limit added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,21 @@
 require_relative 'bike'
-class DockingStation
 
+class DockingStation
+#  bike_capacity_full = false
+
+  # if bike_capacity_full = true
+  #  return "no more bikes can be stored"
+  # else
+  #  subject.dock(bike)e
   attr_reader :bike
 
   def release_bike
+    fail 'No bikes available' unless @bike
     @bike
   end
 
   def dock(bike)
+    fail 'Docking station full' if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,6 +9,13 @@ require 'bike'
 
 describe DockingStation do
 
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
 #  it { is_expected.to respond_to :release_bike }
 
 #it 'gets a bike' do
@@ -32,10 +39,8 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
 end
